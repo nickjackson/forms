a8ec67944fb19123b23247a0cd2f6b8f52cf43e0
Nick Jackson <contact@nickjjackson.com>
2012-12-09 15:47:48 +0000

    adding main object

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Module dependencies.
+ */
+
+
+
+/**
+ * Expose `Form`.
+ */
+
+module.exports = Form;
+
+/**
+ * Initialize a new `Form` with a `schema` object
+ * and prefill with `data` obj
+ *
+ * @param {Object} schema
+ * @param {Object} data
+ * @api public
+ */
+
+function Form(schema, data) {
+  
+}
